8e8c285c6555b96a1dd4f861adf9aa7db2f537ee
programaticrebeca <36053865+programaticrebeca@users.noreply.github.com>
2018-02-23 13:31:52 +0100

    empezando

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Blog</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="cabecera">
+    
+</div>
+<div class="cuerpo">
+    
+</div>
+</body>
+</html>
+
+
+
+
+<?php
+
+?>
