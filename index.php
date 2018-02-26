cbfe2f4bd8b179d0b10a9ae461e2d7e47d80deb5
programaticrebeca <36053865+programaticrebeca@users.noreply.github.com>
2018-02-26 11:55:55 +0100

    parte

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
 
 <body>
 <div class="cabecera">
-    
+    <div class="contenidocabecera">
+        
+    </div>  
 </div>
-<div class="cuerpo">
-    
+<div class="fondo">
+    <div class="contenido">
+        
+    </div>
+</div>
+<div class="pie">
+    <div class="contenidopie">
+        
+    </div>
 </div>
 </body>
 </html>
