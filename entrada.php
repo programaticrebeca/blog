b98a346992ddfcfc2aaff15ffa188796f3a4e43f
programaticrebeca <36053865+programaticrebeca@users.noreply.github.com>
2018-02-27 14:03:01 +0100

    añado entrada.php

---
+++ entrada.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Blog</title>
+    <link href="estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="cabecera">
+    <div class="contenidocabecera">
+        <a href="http://www.ajeclm.com/" target="blank" class="logo"></a>
+        <a href="index.php"><h1>Blog del Programa TIC</h1></a>
+    </div>  
+</div>
+<div class="fondoentrada">
+    <div class="contenidoentrada">
+        <a class="volver" href="index.php"><p>Volver</p></a>
+    </div>
+    <div class="pie">
+        <div class="contenidopie">
+            <a href="https://www.jccm.es/" target="blank">
+                <img class="junta" src="imagenes/comunes/junta.jpg"
+                     alt="logo de la jccm" title="logo de la jccm"/>
+            </a>
+            <a href="http://www.ajeclm.com/programa-tic/" target="blank">
+                <img class="programatic" src="imagenes/comunes/programatic.jpg"
+                     alt="programatic" title="programatic"/>
+            </a>
+            <a href="http://www.ajeclm.com/" target="blank">
+                <img class="aje" src="imagenes/comunes/aje.jpg"
+                     alt="logo de aje" title="logo aje"/>
+            </a>
+            <a href="http://ec.europa.eu/esf/home.jsp?langId=es" target="blank">
+                <img class="europeo" src="imagenes/comunes/europeo.jpg"
+                     alt="logo del fondo social europeo"
+                     title="logo del fondo social europeo"/>
+            </a>
+            
+        </div>
+    </div>  
+</div>
+
+</body>
+</html>
+
+
+
+
+<?php
+
+?>
