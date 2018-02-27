c97f4e4300f44f47d50960d930cc201ff71abcdf
programaticrebeca <36053865+programaticrebeca@users.noreply.github.com>
2018-02-27 13:12:36 +0100

    index sin php

--- index.php
+++ index.php
@@ -15,7 +15,29 @@
 </div>
 <div class="fondo">
     <div class="contenido">
-        
+        <div class="entrada">
+            <div class="contenedorimagen">
+                <img class="imagen" src="imagenes/comunes/junta.jpg"
+                     alt="junta" title="junta"/>
+            </div>
+            <div class="informacion">
+                <p class="fecha">27 de Febrero de 2018</p>
+                <h2 class="titulo">EJEMPLO DE TITULO DE ENTRADA</h2>
+                <p class="texto">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+
+Expetenda tincidunt in sed, ex partem placerat sea, porro commodo ex eam. His putant aeterno interesset at. Usu ea mundi tincidunt, omnium virtute aliquando ius ex. Ea aperiri sententiae duo. Usu nullam dolorum quaestio ei, sit vidit facilisis ea. Per ne impedit iracundia neglegentur. Consetetur neglegentur eum ut, vis animal legimus inimicus id.
+
+His audiam deserunt in, eum ubique voluptatibus te. In reque dicta usu. Ne rebum dissentiet eam, vim omnis deseruisse id. Ullum deleniti vituperata at quo, insolens complectitur te eos, ea pri dico munere propriae. Vel ferri facilis ut, qui paulo ridens praesent ad. Possim alterum qui cu. Accusamus consulatu ius te, cu decore soleat appareat usu.
+
+Est ei erat mucius quaeque. Ei his quas phaedrum, efficiantur mediocritatem ne sed, hinc oratio blandit ei sed. Blandit gloriatur eam et. Brute noluisse per et, verear disputando neglegentur at quo. Sea quem legere ei, unum soluta ne duo. Ludus complectitur quo te, ut vide autem homero pro.
+
+Vis id minim dicant sensibus. Pri aliquip conclusionemque ad, ad malis evertitur torquatos his. Has ei solum harum reprimique, id illum saperet tractatos his. Ei omnis soleat antiopam quo. Ad augue inani postulant mel, mel ea qualisque forensibus.
+		</p>
+                <form action="entrada.php" method="post" enctype="multipart/form-data">
+                   <input type="submit" class="boton leer" value="LEER MAS"/> 
+                </form>
+            </div>
+        </div>
     </div>
     <div class="pie">
         <div class="contenidopie">
